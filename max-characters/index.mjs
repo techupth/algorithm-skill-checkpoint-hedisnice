2e0c56c39a9124a125edeeec3736039e75620da1
iHEDISNICE <hedis_nice@yahoo.com>
2024-02-21 14:33:05 +0700

    refactor : code

--- max-characters/index.mjs
+++ max-characters/index.mjs
@@ -6,18 +6,13 @@
 // getMaxCharacters("abcccccccd") === "c"
 // getMaxCharacters("apple 1231111") === "1"
 
-// ดึงตัวอักษรที่มีจำนวนมากที่สุด
 export const getMaxCharacters = (str) => {
-  // reduce คำนวณจำนวนของแต่ละตัวอักษร   และหาตัวอักษรที่มีจำนวนมากที่สุด
-  const maxChar = str.split("").reduce((charCounts, char) => {
-    charCounts[char] = (charCounts[char] || 0) + 1;
-    return charCounts;
-  }, {});
-
-  // หาตัวอักษรที่มีจำนวนมากที่สุด
-  return Object.keys(maxChar).reduce((a, b) =>
-    maxChar[a] > maxChar[b] ? a : b
-  );
+  return Object.entries(
+    str.split("").reduce((charCounts, char) => {
+      charCounts[char] = (charCounts[char] || 0) + 1;
+      return charCounts;
+    }, {})
+  ).reduce((a, b) => (a[1] > b[1] ? a : b))[0];
 };
 
 console.log(getMaxCharacters("abcccccccd"));
